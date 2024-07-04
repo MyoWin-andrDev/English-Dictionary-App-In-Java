23f3bf7fc1c19b9beaa3122c908546d0188547a1
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-07-04 07:35:52 +0700

    Initial Commit

--- src/JavaDictionary.java
+++ src/JavaDictionary.java
@@ -1,3 +1,7 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Scanner;
 
 public class JavaDictionary {
@@ -9,8 +13,22 @@ public class JavaDictionary {
             System.out.print("Enter a word : ");
             word = scanner.nextLine();
             if(word.equalsIgnoreCase("No")) break;
-
-
+            //Get Data
         }while (!word.equalsIgnoreCase("No"));
     }
 }
+class GetData{
+    public static void main(String[] args) {
+        String word = "";
+        String urlString = "https://api.dictionaryapi.dev/api/v2/entries/en/"+word;
+
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
