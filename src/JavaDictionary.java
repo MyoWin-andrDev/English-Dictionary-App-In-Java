d5e0263019fd65d65c85838bc8e16c9b6e76b24b
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-07-04 07:35:52 +0700

    Initial Commit

--- src/JavaDictionary.java
+++ src/JavaDictionary.java
@@ -1,3 +1,16 @@
-public class JavaDictionary {
+import java.util.Scanner;
 
+public class JavaDictionary {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word ;
+        do{
+            System.out.println("**___Welcome___To___Java___Dictionary___***");
+            System.out.print("Enter a word : ");
+            word = scanner.nextLine();
+            if(word.equalsIgnoreCase("No")) break;
+
+
+        }while (!word.equalsIgnoreCase("No"));
+    }
 }
