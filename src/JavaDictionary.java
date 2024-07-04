966a6dd97ec83505a1b6a22162267ca7c7dafae8
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-07-04 07:35:52 +0700

    Initial Commit

--- src/JavaDictionary.java
+++ src/JavaDictionary.java
@@ -1,7 +1,11 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.Buffer;
 import java.util.Scanner;
 
 public class JavaDictionary {
@@ -12,23 +16,42 @@ public class JavaDictionary {
             System.out.println("**___Welcome___To___Java___Dictionary___***");
             System.out.print("Enter a word : ");
             word = scanner.nextLine();
+            getData(word);
             if(word.equalsIgnoreCase("No")) break;
-            //Get Data
+
+
+
         }while (!word.equalsIgnoreCase("No"));
     }
-}
-class GetData{
-    public static void main(String[] args) {
-        String word = "";
-        String urlString = "https://api.dictionaryapi.dev/api/v2/entries/en/"+word;
 
-        try {
-            URL url = new URL(urlString);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    public static void getData(String word){
+            //API URL
+            String urlString = "https://api.dictionaryapi.dev/api/v2/entries/en/"+word;
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+            try {
+                URL url = new URL(urlString);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                //Requesting to Get data
+                conn.setRequestMethod("GET");
+                int respondCode = conn.getResponseCode();
+                if(respondCode == 200){
+                    BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    String inputLine;
+                    StringBuilder content = new StringBuilder();
+                    while((inputLine = in.readLine()) != null){
+                        content.append(inputLine);
+                    }
+                    in.close();
+                    conn.disconnect();
+                    System.out.println("Successful");
+
+                }else{
+                    System.out.println("Word Not Found! Try Another.");
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
     }
 }
 
